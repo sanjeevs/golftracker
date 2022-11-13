a4c9a8ad4df88eafeb194a249baaf6a330c1e2b8
sanjeevs <snjvsingh123@gmail.com>
2022-11-12 23:56:07 -0500

    fixed clone

--- golftracker/golf_swing_factory.py
+++ golftracker/golf_swing_factory.py
@@ -3,18 +3,18 @@
 #
 import video_utils
 import golf_swing
-
+import copy
 
 def create_from_video(video_fname):
     frames = video_utils.split_video_to_frames(video_fname)
     return golf_swing.GolfSwing(frames)
 
-def clone_new_context(golf_swing):
+def clone_new_context(gs):
     """Return a new golf swing that has a deep copy of frame context.
     However the frames are a shallow copy since they are usually read only
     """
-    gs = golf_swing.GolfSwing(golf_swing.frames)
-    for idx, frame_context in enumerate(golf_swing.frame_contexts):
-        gs.frame_contexts[idx] = copy(frame_context)
+    gs_clone = golf_swing.GolfSwing(gs.frames)
+    for idx, frame_context in enumerate(gs.frame_contexts):
+        gs_clone.frame_contexts[idx] = copy.deepcopy(frame_context)
 
-    return gs
+    return gs_clone
--- tests/test_golf_swing.py
+++ tests/test_golf_swing.py
@@ -1,8 +1,10 @@
 from golftracker import golf_swing
 from golftracker import golf_swing_factory
+from golftracker import frame_tracker
 
 import numpy as np
 import os
+
 
 def test_null_init():
     gs = golf_swing.GolfSwing([])
@@ -18,3 +20,20 @@ def test_blank_video():
     # Ensure deep copy of frame contexts
     gs.frame_contexts[0].frame_tracker["nose"] == (1.0, 2.0)
     assert gs.frame_contexts[1].frame_tracker['nose'] == (0, 0)
+
+def test_clone():
+    fname = os.path.join("tests", "assets", "test_blank_3.mov")
+    gs = golf_swing_factory.create_from_video(fname)
+    gs_clone = golf_swing_factory.clone_new_context(gs)
+
+     # Ensure deep copy of frame contexts
+    ft1 = frame_tracker.FrameTracker({'nose': (1.0, 2.0)})
+    gs.frame_contexts[0].frame_tracker = ft1
+    assert gs_clone.frame_contexts[0].frame_tracker['nose'] == (0, 0)
+    assert gs_clone.frame_contexts[1].frame_tracker['nose'] == (0, 0)
+
+    ft2 = frame_tracker.FrameTracker({'nose': (3.0, 4.0)})
+    gs_clone.frame_contexts[0].frame_tracker = ft2
+    assert gs.frame_contexts[0].frame_tracker['nose'] == (1.0, 2.0)
+    assert gs_clone.frame_contexts[1].frame_tracker['nose'] == (0, 0)
+    
