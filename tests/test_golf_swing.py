d7e33a7936223375d754297e38d46f05ac49b508
sanjeevs <snjvsingh123@gmail.com>
2022-12-16 23:50:29 -0500

    backup

--- tests/test_golf_swing.py
+++ tests/test_golf_swing.py
@@ -13,14 +13,3 @@ def test_null_init():
     assert gs.height == 10
     assert gs.width == 20
     assert gs.num_frames == 30
-
-
-def test_json():
-    gs = golf_swing.GolfSwing(height=10, width=20, num_frames=30)
-    gs.set_ml_pose(10, gt_const.GolfPose.RhStart, 0.3)
-    json_str = gs.to_json()
-   
-    new_gs = golf_swing_factory.create_from_json(json_str)
-    json_str = ""
-    gs = None
-    assert new_gs.num_frames == 30
--- tests/test_golf_swing_factory.py
+++ tests/test_golf_swing_factory.py
@@ -1,21 +1,11 @@
 from golftracker import golf_swing_factory
 import os
+import pickle
 
 def test_create_from_blank_video():
     fname = os.path.join('tests', 'assets', 'test_blank_3.mov')
     gs = golf_swing_factory.create_from_video(fname, pose_model = None)
-    json_str = gs.to_json()
     assert gs.height == 100
     assert gs.width == 200
     assert gs.num_frames == 3
 
-
-def test_create_from_blank_json():
-    fname = os.path.join('tests', 'assets', 'test_blank_3.json')
-    with open(fname, "r") as fh:
-        json_str = fh.read()
-
-    gs = golf_swing_factory.create_from_json(json_str)
-    assert gs.num_frames == 3
-    assert gs.height == 100
-    assert gs.width == 200
